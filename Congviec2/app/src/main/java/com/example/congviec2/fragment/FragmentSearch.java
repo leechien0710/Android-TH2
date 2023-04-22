5dfe610fc528abb666d394f7939fc9a6ec3b9dd8
leechien0710 <chienle07102001@gmail.com>
2023-04-22 08:40:07 +0700

    Android

---
+++ Congviec2/app/src/main/java/com/example/congviec2/fragment/FragmentSearch.java
@@ -0,0 +1,8 @@
+package com.example.congviec2.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentSearch extends Fragment {
+    public FragmentSearch() {
+    }
+}
---
+++ Congviec2/app/src/main/java/com/example/congviec2/fragment/FrangmentInfor.java
@@ -0,0 +1,20 @@
+package com.example.congviec2.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.congviec2.R;
+
+public class FrangmentInfor  extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info,container,false);
+    }
+}
